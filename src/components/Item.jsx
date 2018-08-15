dc554c47f2fbbc0b84afe620c06cbca2e17f292e
Veronika Loukotova <VeronikaL@kentico.com>
2018-08-15 16:31:32 +0200

    KA-349 - Make Save button work
    Enable editing items and saving changes.

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,36 +4,56 @@ export class Item extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      clicked: false,
+      isClicked: false,
+      text: props.text
     };
   }
 
   handleClick = () => {
-    this.setState({ clicked: true });
+    this.setState(prevState => ({ isClicked: !prevState.isClicked }));
   };
 
   handleCancel = () => {
-    this.setState((() => ({ clicked: false })));
+    this.setState((() => ({ isClicked: false })));
+  }
+
+  handleEditText = (event) => {
+    this.setState({ text: event.target.value });
+  }
+
+  handleSave = () => {
+    const { onEdit, id } = this.props;
+    const { text } = this.state;
+    onEdit(id, text);
+    this.setState((() => ({ isClicked: false })));
+  }
+
+  editInput = (text) => {
+    return (
+      <div className="input-group" style={{ width: 420 }}>
+        <input type="text" defaultValue={text} className="form-control" onChange={this.handleEditText}/>
+        {this.state.isClicked ? this.showButtons() : undefined}
+      </div>
+    );
   }
 
   showButtons = () => {
     const { onDelete, id } = this.props;
     return (
       <span>
-        <button type="button" className="btn btn-primary btn-sm" color="primary">Save</button>
-        <button type="button" className="btn btn-light btn-sm" onClick={this.handleCancel}>Cancel</button>
-        <button type="button" className="btn btn-danger btn-sm" onClick={() => onDelete(id)}>Delete</button>
+        <button type="button" className="btn btn-primary" color="primary" onClick={this.handleSave} disabled={!this.state.text.trim()}>Save</button>
+        <button type="button" className="btn btn-light" onClick={this.handleCancel}>Cancel</button>
+        <button type="button" className="btn btn-danger" onClick={() => onDelete(id)}>Delete</button>
       </span>
     );
   }
 
   render() {
     const { text } = this.props;
-
+    const { isClicked } = this.state;
     return (
       <li className="list-group-item" >
-        <a onClick={this.handleClick}> {text} </a>
-        {this.state.clicked ? this.showButtons() : undefined}
+        {isClicked ? this.editInput(text) : (<a onClick={this.handleClick}> {text} </a>)}
       </li>
     );
   }
