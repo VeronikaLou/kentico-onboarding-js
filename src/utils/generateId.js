bd1e2f0f644c9cd089bd3ed8acb7ddb9e29d421d
Veronika Loukotova <VeronikaL@kentico.com>
2018-08-20 09:54:45 +0200

    KA-349 - Improve generateId function
    Previous function replaced with function from npm uuid package.

--- src/utils/generateId.js
+++ src/utils/generateId.js
@@ -1,8 +1,5 @@
+import uuidv4 from 'uuid/v4';
+
 export const generateId = () => {
-  const s4 = () => {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  };
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  return uuidv4();
 };
