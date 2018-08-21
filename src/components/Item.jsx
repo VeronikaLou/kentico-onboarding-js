ff52dbdfa36e7cc9ff0dca3cb1ca76c36c39b62c
Veronika Loukotova <VeronikaL@kentico.com>
2018-08-21 09:11:57 +0200

    KA-349 - Make Item component stateless

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,33 +1,27 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { EditedItem } from './EditedItem';
 import { PlainItem } from './PlainItem';
 
-export class Item extends PureComponent {
-  render() {
-    const {
-      index, saveChanges, changeEditingMode, deleteItem, item
-    } = this.props;
-
-    return (
-      <li className="list-group-item">
-        {item.isEdited
-          ? (
-            <EditedItem
-              index={index}
-              item={item}
-              saveChanges={saveChanges}
-              cancelEditing={changeEditingMode}
-              deleteItem={deleteItem}
-            />
-          )
-          : (
-            <PlainItem
-              startEditing={changeEditingMode}
-              index={index}
-              item={item}
-            />
-          )}
-      </li>
-    );
-  }
-}
+export const Item = ({
+  index, saveChanges, changeEditingMode, deleteItem, item
+}) => (
+  <li className="list-group-item">
+    {item.isEdited
+      ? (
+        <EditedItem
+          index={index}
+          item={item}
+          saveChanges={saveChanges}
+          cancelEditing={changeEditingMode}
+          deleteItem={deleteItem}
+        />
+      )
+      : (
+        <PlainItem
+          startEditing={changeEditingMode}
+          index={index}
+          item={item}
+        />
+      )}
+  </li>
+);
