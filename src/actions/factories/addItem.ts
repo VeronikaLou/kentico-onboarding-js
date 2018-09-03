ac3fbac97fb02471f9fc8aaf82662f1e2309aa12
Veronika Loukotova <VeronikaL@kentico.com>
2018-09-03 14:13:27 +0200

    KA-352 - Rewrite actions to TS

--- src/actions/factories/addItem.ts
+++ src/actions/factories/addItem.ts
@@ -1,6 +1,7 @@
 import { ITEM_ADDED } from '../types/listActionTypes';
+import { IListAction } from '../IListAction';
 
-export const addItem = (generator) => (text) => ({
+export const addItem = (generator: Function) => (text: string): IListAction => ({
   type: ITEM_ADDED,
   payload: {
     id: generator(),
--- src/actions/listActionCreators.ts
+++ src/actions/listActionCreators.ts
@@ -1,21 +1,22 @@
-import { generateId } from '../utils/generateId.ts';
+import { generateId, Uuid } from '../utils/generateId';
 import {
   ITEM_CHANGES_SAVED,
   ITEM_EDITING_MODE_CHANGED,
   ITEM_DELETED
 } from './types/listActionTypes';
 import { addItem as addItemFactory } from './factories/addItem';
+import { IListAction } from './IListAction';
 
-export const addItem = (text) => addItemFactory(generateId)(text);
+export const addItem = (text: string): IListAction => addItemFactory(generateId)(text);
 
-export const deleteItem = id => ({
+export const deleteItem = (id: Uuid): IListAction => ({
   type: ITEM_DELETED,
   payload: {
     id
   }
 });
 
-export const saveItemChanges = (id, text) => ({
+export const saveItemChanges = (id: Uuid, text: string): IListAction => ({
   type: ITEM_CHANGES_SAVED,
   payload: {
     id,
@@ -23,7 +24,7 @@ export const saveItemChanges = (id, text) => ({
   }
 });
 
-export const changeItemEditingMode = id => ({
+export const changeItemEditingMode = (id: Uuid): IListAction => ({
   type: ITEM_EDITING_MODE_CHANGED,
   payload: {
     id
