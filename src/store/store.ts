3647d8502509054dcbc5dbd1c22879d70e05f8ec
Veronika Loukotova <VeronikaL@kentico.com>
2018-09-10 15:31:29 +0200

    KA-352 - Create ItemsState type

--- src/store/store.ts
+++ src/store/store.ts
@@ -8,7 +8,7 @@ import { logger } from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createItems } from './itemsCreator';
 import { applicationReducer } from '../reducers/applicationReducer';
-import { IStore } from '../models/IStore';
+import { IStore } from '../store/types/IStore';
 
 const enhancer: StoreEnhancer = composeWithDevTools(
   applyMiddleware(logger),
