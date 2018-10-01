0afcd680fbf4291f7d39396b584aa126f3c29a4f
Veronika Loukotova <VeronikaL@kentico.com>
2018-10-01 09:22:33 +0200

    KA-358 - Edit listActionTypes, listActionCreators
    Create types ITEMS_REQUESTED, ITEMS_RECEIVED and actions receiveItems, requestItems.

--- src/actions/listActionCreators.ts
+++ src/actions/listActionCreators.ts
@@ -1,13 +1,16 @@
-import { generateId } from '../utils/generateId';
 import {
   ITEM_CHANGES_SAVED,
   ITEM_DELETED,
   ITEM_EDITING_MODE_CHANGED,
+  ITEMS_RECEIVED,
+  ITEMS_REQUESTED,
 } from './types/listActionTypes';
 import { addItem as addItemFactory } from './factories/addItem';
 import { IListAction } from './types/IListAction';
+import { ListItem } from '../models/ListItem';
+import { OrderedMap } from 'immutable';
 
-export const addItem = addItemFactory(generateId);
+export const addItem = (id: Uuid, text: string): IListAction => addItemFactory(() => id)(text);
 
 export const deleteItem = (id: Uuid): IListAction => ({
   type: ITEM_DELETED,
@@ -29,4 +32,16 @@ export const changeItemEditingMode = (id: Uuid): IListAction => ({
   payload: {
     id,
   },
+});
+
+export const receiveItems = (items: OrderedMap<Uuid, ListItem>): IListAction => ({
+  type: ITEMS_RECEIVED,
+  payload: {
+    items,
+  },
+});
+
+export const requestItems = (): IListAction => ({
+  type: ITEMS_REQUESTED,
+  payload: {},
 });
