bd871eef3b34e2fc211a710e820bfea0e90dfc6f
Veronika Loukotova <VeronikaL@kentico.com>
2018-10-24 15:42:50 +0200

    KA-358 - Edit item and items reducers

--- src/reducers/list/item.ts
+++ src/reducers/list/item.ts
@@ -1,20 +1,48 @@
 import { ListItem } from '../../models/ListItem';
 import {
-  ITEM_ADDED,
-  ITEM_CHANGES_SAVED,
+  CLOSE_DELETE_ERROR,
+  CLOSE_SAVE_ERROR,
+  ITEM_ADD_FAIL,
+  ITEM_ADD_REQUESTED,
+  ITEM_ADD_SUCCESS,
+  ITEM_DELETE_FAIL,
+  ITEM_DELETE_REQUESTED,
   ITEM_EDITING_MODE_CHANGED,
+  ITEM_SAVE_FAIL,
+  ITEM_SAVE_REQUESTED,
+  ITEM_SAVE_SUCCESS,
 } from '../../actions/types/listActionTypes';
 import { IListAction } from '../../actions/types/IListAction';
 
 export const item = (state: ListItem = new ListItem(), action: IListAction): ListItem => {
   switch (action.type) {
-    case ITEM_ADDED:
-    case ITEM_CHANGES_SAVED:
-      return new ListItem({...action.payload});
+    case ITEM_ADD_SUCCESS:
+      return state
+        .with({id: action.payload.fetchedId, isUpdating: false});
+
+    case ITEM_DELETE_REQUESTED:
+      return state
+        .with({isUpdating: true, isEdited: false});
+
+    case ITEM_SAVE_SUCCESS:
+    case CLOSE_DELETE_ERROR:
+      return state
+        .with({isUpdating: false});
 
     case ITEM_EDITING_MODE_CHANGED:
       return state
         .with({isEdited: !state.isEdited});
+
+    case ITEM_ADD_FAIL:
+    case ITEM_SAVE_FAIL:
+    case ITEM_DELETE_FAIL:
+      return state
+        .with({isEdited: false, isUpdating: false});
+
+    case ITEM_ADD_REQUESTED:
+    case ITEM_SAVE_REQUESTED:
+    case CLOSE_SAVE_ERROR:
+      return new ListItem({...action.payload});
 
     default:
       return state;
--- src/reducers/list/items.ts
+++ src/reducers/list/items.ts
@@ -1,11 +1,19 @@
 import { item } from './item';
 import {
-  ITEM_ADDED,
-  ITEM_CHANGES_SAVED,
-  ITEM_DELETED,
-  ITEM_EDITING_MODE_CHANGED, ITEMS_RECEIVED,
+  CLOSE_ADD_ERROR, CLOSE_DELETE_ERROR,
+  CLOSE_SAVE_ERROR,
+  ITEM_ADD_FAIL,
+  ITEM_ADD_REQUESTED,
+  ITEM_ADD_SUCCESS,
+  ITEM_DELETE_FAIL,
+  ITEM_DELETE_REQUESTED,
+  ITEM_DELETE_SUCCESS,
+  ITEM_EDITING_MODE_CHANGED,
+  ITEM_SAVE_FAIL,
+  ITEM_SAVE_REQUESTED,
+  ITEM_SAVE_SUCCESS,
+  ITEMS_RECEIVE_SUCCESS,
 } from '../../actions/types/listActionTypes';
-import { ListItem } from '../../models/ListItem';
 import { IListAction } from '../../actions/types/IListAction';
 import { OrderedMap } from 'immutable';
 import { ItemsState } from '../../store/types/ItemsState';
@@ -13,26 +21,41 @@ import { ItemsState } from '../../store/types/ItemsState';
 export const items = (
   state: ItemsState = OrderedMap(),
   action: IListAction,
-): OrderedMap<Uuid, ListItem> => {
+): ItemsState => {
   switch (action.type) {
-    case ITEM_ADDED:
-    case ITEM_CHANGES_SAVED:
-      return state
-        .set(action.payload.id, item(undefined, action));
-
-    case ITEM_DELETED:
+    case ITEM_DELETE_SUCCESS:
+    case CLOSE_ADD_ERROR:
       return state
         .delete(action.payload.id);
 
+    case ITEMS_RECEIVE_SUCCESS:
+      return action.payload.items;
+
+    case ITEM_ADD_SUCCESS:
+      return state
+        .set(
+          action.payload.fetchedId,
+          item(state.get(action.payload.id), action))
+        .delete(action.payload.id);
+
+    case ITEM_ADD_REQUESTED:
+    case ITEM_SAVE_REQUESTED:
+    case CLOSE_SAVE_ERROR:
+      return state
+        .set(action.payload.id, item(undefined, action));
+
+    case ITEM_SAVE_FAIL:
+    case ITEM_ADD_FAIL:
+    case ITEM_DELETE_FAIL:
+    case ITEM_DELETE_REQUESTED:
     case ITEM_EDITING_MODE_CHANGED:
+    case ITEM_SAVE_SUCCESS:
+    case CLOSE_DELETE_ERROR:
       return state
         .set(
           action.payload.id,
           item(state.get(action.payload.id), action),
         );
-
-     case ITEMS_RECEIVED:
-      return action.payload.items;
 
     default:
       return state;
