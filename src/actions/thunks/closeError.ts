f5e45a47e961d103cc1b4013ca3bfbbba738b2ce
Veronika Loukotova <VeronikaL@kentico.com>
2018-11-19 12:56:36 +0100

    KA-358 - Edit retry, closeError
    Rename retry function to retryFactory

--- src/actions/thunks/closeError.ts
+++ src/actions/thunks/closeError.ts
@@ -2,36 +2,43 @@ import { ListError } from '../../models/ListError';
 import {
   ITEM_ADD_FAIL,
   ITEM_DELETE_FAIL,
-  ITEM_SAVE_CLOSE,
-  ITEM_SAVE_FAIL
+  CLOSE_SAVE_ERROR,
+  ITEM_SAVE_FAIL, CLOSE_DELETE_ERROR, CLOSE_ADD_ERROR,
 } from '../types/listActionTypes';
 import { Dispatch } from '../types/Dispatcher';
 import { IListAction } from '../types/IListAction';
-import { deleteItemSuccess, saveItemSuccess } from '../listActionCreators';
 
-export const closeError = (error: ListError, backupText: string):
-  ((dispatch: Dispatch<IListAction>) => IListAction | undefined) => {
-  return (dispatch: Dispatch<IListAction>) => {
-    switch (error.action) {
-      case ITEM_DELETE_FAIL:
-        return dispatch(saveItemSuccess(error.itemId));
-
-      case ITEM_ADD_FAIL:
-        return dispatch(deleteItemSuccess(error.itemId));
-
-      case ITEM_SAVE_FAIL:
-        return dispatch(closeSaveItem(error.itemId, backupText));
-
-      default:
-        return;
-    }
-  };
-};
-
-const closeSaveItem = (id: Uuid, text: string): IListAction => ({
-  type: ITEM_SAVE_CLOSE,
+export const closeSaveError = (id: Uuid, text: string): IListAction => ({
+  type: CLOSE_SAVE_ERROR,
   payload: {
     id,
     text,
   },
 });
+
+export const closeDeleteError = (id: Uuid): IListAction => ({
+  type: CLOSE_DELETE_ERROR,
+  payload: {id},
+});
+
+export const closeAddError = (id: Uuid): IListAction => ({
+  type: CLOSE_ADD_ERROR,
+  payload: {id},
+});
+
+export const closeError = (error: ListError, backupText: string) =>
+  (dispatch: Dispatch<IListAction>): IListAction => {
+    switch (error.action) {
+      case ITEM_DELETE_FAIL:
+        return dispatch(closeDeleteError(error.itemId));
+
+      case ITEM_ADD_FAIL:
+        return dispatch(closeAddError(error.itemId));
+
+      case ITEM_SAVE_FAIL:
+        return dispatch(closeSaveError(error.itemId, backupText));
+
+      default:
+        throw 'Invalid action was dispatched.';
+    }
+  };
--- src/actions/thunks/retry.ts
+++ src/actions/thunks/retry.ts
@@ -3,26 +3,29 @@ import { ITEM_ADD_FAIL, ITEM_DELETE_FAIL, ITEM_SAVE_FAIL } from '../types/listAc
 import { Dispatch } from '../types/Dispatcher';
 import { IListAction } from '../types/IListAction';
 import { IStore } from '../../store/types/IStore';
-import { postItem } from './postItem';
-import { putItem } from './putItem';
-import { deleteItem } from './deleteItem';
 
-export const retry = (error: ListError): any => {
-  return (dispatch: Dispatch<IListAction>, getState: () => IStore) => {
-    const item = getState().items.get(error.itemId);
-    switch (error.action) {
-      case ITEM_DELETE_FAIL:
-        return dispatch(deleteItem(item.id));
+interface IRetryActions {
+  readonly deleteItem: (id: Uuid) => ((dispatch: Dispatch<IListAction>) => Promise<IListAction>);
+  readonly postItem: (text: string, id: Uuid) => ((dispatch: Dispatch<IListAction>) => Promise<IListAction>);
+  readonly putItem: (id: Uuid, text: string, backupText: string) => ((Dispatch: Dispatch<IListAction>) => Promise<IListAction>);
+}
 
-      case ITEM_ADD_FAIL:
-        return dispatch(postItem(item.text, item.id));
+export const retryFactory =
+  ({deleteItem, postItem, putItem}: IRetryActions) =>
+    (error: ListError) =>
+      (dispatch: Dispatch<IListAction>, getState: () => IStore): Promise<IListAction> => {
+        const item = getState().items.get(error.itemId);
+        switch (error.action) {
+          case ITEM_DELETE_FAIL:
+            return dispatch(deleteItem(item.id));
 
-      case ITEM_SAVE_FAIL:
-        return dispatch(putItem(item.id, item.text, item.backupText));
+          case ITEM_ADD_FAIL:
+            return dispatch(postItem(item.text, item.id));
 
-      default:
-        return;
-    }
-  };
-};
+          case ITEM_SAVE_FAIL:
+            return dispatch(putItem(item.id, item.text, item.backupText));
 
+          default:
+            throw 'Invalid action was dispatched.';
+        }
+      };
