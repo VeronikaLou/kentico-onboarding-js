59dea574b141f0d8bd94639fb2f4a6cfac4d5ead
Veronika Loukotova <VeronikaL@kentico.com>
2018-11-21 15:10:23 +0100

    KA-358 - Add error and isUpdating to ListItem

--- src/models/ListItem.ts
+++ src/models/ListItem.ts
@@ -4,16 +4,22 @@ interface IListItem {
   readonly id: Uuid;
   readonly text: string;
   readonly isEdited: boolean;
+  readonly isUpdating: boolean;
+  readonly error: string | undefined;
 }
 
 const emptyItem: IListItem = {
   id: '00000000-0000-0000-0000-000000000000',
   text: '',
   isEdited: false,
+  isUpdating: false,
+  error: undefined,
 };
 
 export class ListItem extends BaseRecord(emptyItem, 'ListItem') implements IListItem {
   readonly id: Uuid;
   readonly text: string;
   readonly isEdited: boolean;
+  readonly isUpdating: boolean;
+  readonly error: string;
 }
