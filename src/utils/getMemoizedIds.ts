e2fc36ff3e72ead6756a840ce55604592765f3a7
Veronika Loukotova <VeronikaL@kentico.com>
2019-02-18 10:46:59 +0100

    KA-358 - Edit getMemoizedIds

--- src/utils/getMemoizedIds.ts
+++ src/utils/getMemoizedIds.ts
@@ -2,5 +2,8 @@ import * as memoizee from 'memoizee';
 
 export const getMemoizedIds = memoizee(
   (items: Array<Uuid>) => items,
-  {primitive: true},
+  {
+    primitive: true,
+    max: 1,
+  },
 );
